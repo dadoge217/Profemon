f819329834a6e617838a43aab0b500c929e775c1
Colin <crgrant24@gmail.com>
2026-04-12 16:35:28 -0400

    added basic turn logic

--- profs.py
+++ profs.py
@@ -1,10 +1,10 @@
 class Profemon:
-    def __init__(self, name, move1, move2, move3, stats, HP, attack, defense, speed, img=""):
+    def __init__(self, name, move1, move2, move3, type, HP, attack, defense, speed, img=""):
         self.name = name
         self.move1 = move1
         self.move2 = move2
         self.move3 = move3
-        self.stats = stats
+        self.type = type
         self.HP = HP
         self.attack = attack
         self.defense = defense
@@ -18,8 +18,11 @@ class Move:
         self.status = status
 
 class Trainer:
-    def __init__(self, name, prof1, prof2, prof3):
+    def __init__(self, prof1 = "none", prof2 = "none", prof3 = "none", name = "player"):
         self.name = name
         self.prof1 = prof1
         self.prof2 = prof2
         self.prof2 = prof3
+        self.currentProf = prof1
+
+player = Trainer()
